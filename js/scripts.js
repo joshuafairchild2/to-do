fd6125ecd8126c0962dbc801a7011aacb3644d68
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-03-16 12:20:41 -0700

    make the border styling for each task item carry over to the complete section once the item is marked complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 //business logic
+//constructor for object that will contain the information assoocaiated with a single 'to-do task'
 function Task(description, deadline, importance) {
   this.description = description;
   this.deadline = deadline;
@@ -7,36 +8,65 @@ function Task(description, deadline, importance) {
 
 //ui logic
 $(function() {
+  //initialize variables used to act as a running count of how many 'task-items' or 'completed-tasks' have been created. The number of the variable is used for naming classes, making it easier to target a specific task-item/completeted-task
   var itemCount = 0;
+  var completedCount = 0
   $('form#task-form').submit(function(event) {
+    event.preventDefault();
+    //get the input for each field (when the form is submitted)
     var description = $('input#add-task').val();
     var deadline = $('input#add-deadline').val();
     var importance = parseInt($('select#add-importance').val());
-    var toDoTask = new Task(description, deadline, importance);
-    console.log(toDoTask);
-    itemCount += 1;
-    $('div#to-do-tasks').prepend('<div class="well task item' + itemCount + '">' + 'I need to ' + toDoTask.description + ' before ' + toDoTask.deadline +
-                                    '<button class="btn mark-complete item' + itemCount + '">' +
-                                      '<span class="glyphicon glyphicon-ok"></span>' +
-                                    '</button>' +
-                                  '</div>');
-    if (toDoTask.importance === 1) {
-      $('div.item' + itemCount).addClass('yellow-bg');
-    } else if (toDoTask.importance === 2) {
-      $('div.item' + itemCount).addClass('orange-bg');
-    } else if (toDoTask.importance === 3) {
-      $('div.item' + itemCount).addClass('red-bg');
+    //error message if any field is left blank
+    if (!description || !deadline || !importance) {
+      alert('Please fill out all fields');
+    } else {
+      //store a new Task object inside of var toDoTask and increase the running count of task-items by 1
+      var toDoTask = new Task(description, deadline, importance);
+      itemCount += 1;
+      //print an html <div> into the DOM containing some of the data from the Task as well as a button to mark the task as complete
+      $('div#to-do-tasks').prepend('<div class="well task item' + itemCount + '">' + 'I need to ' + toDoTask.description + ' before ' + toDoTask.deadline +
+                                      '<button class="btn mark-complete item' + itemCount + '">' +
+                                        '<span class="glyphicon glyphicon-ok"></span>' +
+                                      '</button>' +
+                                    '</div>');
+      //determine what color the border of the <div> will be based on the value of toDoTask.importance and add the corresponding class
+      if (toDoTask.importance === 1) {
+        $('div.item' + itemCount).addClass('yellow-border');
+      } else if (toDoTask.importance === 2) {
+        $('div.item' + itemCount).addClass('orange-border');
+      } else if (toDoTask.importance === 3) {
+        $('div.item' + itemCount).addClass('red-border');
+      }
+      //clear the form fields
+      $('form#task-form').trigger('reset');
+      //attach a click event handler to the button inside of the Task item
+      $('button.item' + itemCount).click(function() {
+        //increase the running count of completed-tasks by 1
+        completedCount +=1;
+        //hide the task-item of the button that was clicked and print an html <div> into the DOM containing the same information as the hidden task-item
+        $(this).parent().hide();
+        $('div#completed-tasks').prepend('<div class="well task completed-task' + completedCount + '">' + 'I need to ' + toDoTask.description + ' before ' + toDoTask.deadline +
+                                            '<button class="btn delete-item">' +
+                                              '<span class="glyphicon glyphicon-remove-circle"></span>' +
+                                            '</button>' +
+                                          '</div>');
+        //determine what the border color of the completed-task div needs to be and add the corresponding class
+        // if ($(this).parent().hasClass('yellow-bg')) {
+        //   $('div.completed-task' + completedCount).addClass('yellow-bg');
+        // } else if ($(this).parent().hasClass('orange-bg')) {
+        //   $('div.completed-task' + completedCount).addClass('orange-bg');
+        // } else if ($(this).parent().hasClass('red-bg')) {
+        //   $('div.completed-task' + completedCount).addClass('red-bg');
+        // }
+        if (toDoTask.importance === 1) {
+          $('div.completed-task' + completedCount).addClass('yellow-border');
+        } else if (toDoTask.importance === 2) {
+          $('div.completed-task' + completedCount).addClass('orange-border');
+        } else if (toDoTask.importance === 3) {
+          $('div.completed-task' + completedCount).addClass('red-border');
+        }
+      });
     }
-    $('form#task-form').trigger('reset');
-
-    $('button.item' + itemCount).click(function() {
-      $(this).parent().hide();
-      $('div#completed-tasks').prepend('<div class="well completed-task">' + 'I need to ' + toDoTask.description + ' before ' + toDoTask.deadline +
-                                          '<button class="btn delete-item">' +
-                                            '<span class="glyphicon glyphicon-remove-circle"></span>' +
-                                          '</button>' +
-                                        '</div>');
-    });
-    event.preventDefault();
   });
 });
