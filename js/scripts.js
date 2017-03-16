ba0dd6d7239aeb68fda4b38da8e8ae7825c48243
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-03-16 10:42:23 -0700

    add additional input fields to gather more data from the user, custom styling that is determined based on the user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,42 @@
 //business logic
-function Task(input) {
-  this.description = input;
+function Task(description, deadline, importance) {
+  this.description = description;
+  this.deadline = deadline;
+  this.importance = importance;
 }
 
 //ui logic
 $(function() {
   var itemCount = 0;
   $('form#task-form').submit(function(event) {
-    var input = $('input#add-task').val();
-    if (input) {
-    var toDoTask = new Task(input);
+    var description = $('input#add-task').val();
+    var deadline = $('input#add-deadline').val();
+    var importance = parseInt($('select#add-importance').val());
+    var toDoTask = new Task(description, deadline, importance);
+    console.log(toDoTask);
     itemCount += 1;
-    $('div#to-do-tasks').prepend('<div class="well task">' + toDoTask.description +
+    $('div#to-do-tasks').prepend('<div class="well task item' + itemCount + '">' + 'I need to ' + toDoTask.description + ' before ' + toDoTask.deadline +
                                     '<button class="btn mark-complete item' + itemCount + '">' +
                                       '<span class="glyphicon glyphicon-ok"></span>' +
                                     '</button>' +
                                   '</div>');
+    if (toDoTask.importance === 1) {
+      $('div.item' + itemCount).addClass('yellow-bg');
+    } else if (toDoTask.importance === 2) {
+      $('div.item' + itemCount).addClass('orange-bg');
+    } else if (toDoTask.importance === 3) {
+      $('div.item' + itemCount).addClass('red-bg');
+    }
     $('form#task-form').trigger('reset');
 
     $('button.item' + itemCount).click(function() {
       $(this).parent().hide();
-      $('div#completed-tasks').prepend('<div class="well completed-task">' + toDoTask.description +
-                                          '<button class="btn delete">' +
+      $('div#completed-tasks').prepend('<div class="well completed-task">' + 'I need to ' + toDoTask.description + ' before ' + toDoTask.deadline +
+                                          '<button class="btn delete-item">' +
                                             '<span class="glyphicon glyphicon-remove-circle"></span>' +
                                           '</button>' +
                                         '</div>');
-        });
-
-  } else {
-    alert('Please enter something');
-  }
+    });
     event.preventDefault();
   });
 });
