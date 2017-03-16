f2ab9ef327b9c4d14613060545f8cdca63567723
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-03-16 09:04:21 -0700

    add js ui logic to display tasks in a different area after they are marked as complete

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,27 @@ function Task(input) {
 
 //ui logic
 $(function() {
-  var items = 0;
+  var itemCount = 0;
   $('form#task-form').submit(function(event) {
     var input = $('input#add-task').val();
     if (input) {
     var toDoTask = new Task(input);
-    items += 1;
-    $('#to-do-tasks').prepend('<div class="well task item' + items + '">' + toDoTask.description +
-                                '<button class="btn mark-complete item' + items + '">' +
-                                '<span class="glyphicon glyphicon-ok"></span>' +
-                                '</button>' +
-                              '</div>');
+    itemCount += 1;
+    $('div#to-do-tasks').prepend('<div class="well task">' + toDoTask.description +
+                                    '<button class="btn mark-complete item' + itemCount + '">' +
+                                      '<span class="glyphicon glyphicon-ok"></span>' +
+                                    '</button>' +
+                                  '</div>');
     $('form#task-form').trigger('reset');
 
-    $('button.item' + items).click(function() {
+    $('button.item' + itemCount).click(function() {
       $(this).parent().hide();
-    });
+      $('div#completed-tasks').prepend('<div class="well completed-task">' + toDoTask.description +
+                                          '<button class="btn delete">' +
+                                            '<span class="glyphicon glyphicon-remove-circle"></span>' +
+                                          '</button>' +
+                                        '</div>');
+        });
 
   } else {
     alert('Please enter something');
